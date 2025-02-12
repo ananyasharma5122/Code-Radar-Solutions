9515e86b48bcc14304876e277c77f9ae33c378c1
ananyasharma5122 <ananya0258.becse24@chitkara.edu.in>
2025-02-12 11:45:44 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Input two numbers
+    scanf("%d %d", &num1, &num2);
+
+    // Check condition
+    if (num1 >= num2) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+
+    return 0;
+}
