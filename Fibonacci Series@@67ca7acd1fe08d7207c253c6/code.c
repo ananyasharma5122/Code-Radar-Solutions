2db6703998ae3b1b820015d144c23497a731970e
ananyasharma5122 <ananya0258.becse24@chitkara.edu.in>
2025-03-09 23:31:13 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,18 +1,5 @@
 
-    int n;
-
-    printf("Enter the number of terms: ");
-    if (scanf("%d", &n) != 1 || n <= 0) {  // Validate user input
-        printf("Please enter a valid positive integer.\n");
-        return 1;
-    }
-
-    fibonacci(n);  // Call the function
-
-    return 0;
-}
-
-void fibonacci(int n) {
+    void fibonacci(int n) {
     int a = 0, b = 1, next;
 
     printf("Fibonacci Series: ");
@@ -24,4 +11,3 @@ void fibonacci(int n) {
     }
     printf("\n");
 }
-
